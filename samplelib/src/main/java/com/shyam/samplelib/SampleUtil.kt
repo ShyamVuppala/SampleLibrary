3bf72b95bf842685203ad82636d211f417cf5ed3
Shyam Vuppala <shyam.vuppala@paysafe.com>
2021-05-21 13:07:48 +0530

    Added library as subtree and modifying lib

---
+++ samplelib/src/main/java/com/shyam/samplelib/SampleUtil.kt
@@ -0,0 +1,9 @@
+package com.shyam.samplelib
+
+class SampleUtil {
+    companion object {
+        fun getSampleText(): String {
+            return "Sample text from Common Util"
+        }
+    }
+}
